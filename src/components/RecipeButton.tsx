a622a56eea08f7e0916afed283c67527880aa0c0
Zuma <zuma@zuma.eu.org>
2023-09-29 00:14:20 +0100

    Fixed recipe summary

--- src/components/RecipeButton.tsx
+++ src/components/RecipeButton.tsx
@@ -12,14 +12,23 @@ export default function RecipeButton(props: Props) {
   const summary = useMemo(
     () =>
       props.recipe.requirements
-        .map((requirement) => select(requirement.foreignKey, props.stock))
+        .map((requirement) => select(requirement.foreignKey, props.stock)?.name)
         .filter((ingredient) => ingredient)
-        .toString(),
+        .reduce((acc, requirement, index) => {
+          return (
+            (typeof acc == "string" ? acc : "") +
+            (index != 0 ? ", " : "") +
+            requirement
+          );
+        }),
     [props.recipe, props.stock],
   );
 
   return (
-    <Link className="rounded-md bg-gray-100 p-2 shadow-md" to="#">
+    <Link
+      className="rounded-md bg-gray-100 p-2 shadow-md"
+      to={props.recipe.key}
+    >
       <h1 className="text-2xl font-bold">{props.recipe.name}</h1>
       <p className="relative overflow-x-hidden whitespace-nowrap opacity-50 before:absolute before:h-full before:w-full before:bg-gradient-to-r before:from-transparent before:from-80% before:to-gray-100">
         {summary}
