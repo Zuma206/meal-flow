87c384a5458779b6a2964d1e49e49dcba494e463
Zuma <zuma@zuma.eu.org>
2023-09-29 01:24:44 +0100

    Added (bugged) flow generation

--- src/pages/FlowPage.tsx
+++ src/pages/FlowPage.tsx
@@ -3,9 +3,11 @@ import Day from "@/components/Day";
 import Loader from "@/components/Loader";
 import Title from "@/components/Title";
 import db, { select } from "@/lib";
+import { useModal } from "@/lib/modal-context";
 import { useRecipies } from "@/lib/queries";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { FiFilePlus } from "react-icons/fi";
+import { shuffle } from "d3-array";
 
 const dayKeys = [
   "Monday",
@@ -26,6 +28,33 @@ export default function FlowPage() {
   });
 
   const recipes = useRecipies();
+  const queryClient = useQueryClient();
+
+  const generateDays = useMutation({
+    async mutationFn() {
+      if (!recipes.isSuccess) throw new Error();
+      const shuffled = shuffle(recipes.data.items);
+      for (let i = 0; i < dayKeys.length; i++) {
+        const recipe = shuffled[i];
+        await db.days.put(
+          {
+            foreignKey: recipe.key,
+            completed: false,
+          },
+          dayKeys[i],
+        );
+        shuffled.push(recipe);
+      }
+    },
+    onSuccess() {
+      generateDaysModal.close();
+      queryClient.invalidateQueries({ queryKey: ["days"] });
+    },
+  });
+  const generateDaysModal = useModal(generateDays, {
+    prompt:
+      "Are you sure you want to generate a new flow and overwrite the old one?",
+  });
 
   return (
     <>
@@ -33,7 +62,7 @@ export default function FlowPage() {
       {(days.isLoading || recipes.isLoading) && <Loader />}
       {days.isSuccess && recipes.isSuccess && (
         <>
-          <Button>
+          <Button onClick={generateDaysModal.open}>
             <FiFilePlus /> Generate Flow
           </Button>
           <div className="flex justify-center">
