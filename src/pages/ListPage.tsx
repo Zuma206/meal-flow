53e02844b775069bfb6a880bc278c97539ff1a89
Zuma <zuma@zuma.eu.org>
2023-09-30 13:08:33 +0100

    Added adding to stock

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,14 +1,17 @@
+import Alert from "@/components/Alert";
 import Button from "@/components/Button";
 import Loader from "@/components/Loader";
+import Modal from "@/components/Modal";
 import ShoppingListItem, {
   type ShoppingListReq,
 } from "@/components/ShoppingListItem";
 import Title from "@/components/Title";
-import { select } from "@/lib";
+import db, { select } from "@/lib";
 import { useDays, useRecipies, useStock } from "@/lib/queries";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
-import { FiShoppingCart } from "react-icons/fi";
+import { FiShoppingCart, FiSmile } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
 
 export default function ListPage() {
   const stock = useStock();
@@ -20,7 +23,7 @@ export default function ListPage() {
     queryKey: ["stock", "days", "recipes"],
     queryFn() {
       if (!isSuccess) throw new Error();
-      const shoppingList: ShoppingListReq[] = [];
+      let shoppingList: ShoppingListReq[] = [];
       days.data.items.forEach((day) => {
         const recipe = select(day.foreignKey, recipes.data.items);
         if (!recipe) return;
@@ -31,7 +34,7 @@ export default function ListPage() {
           if (!listReq) {
             listReq = {
               key: ingredient.key,
-              count: 0,
+              count: -ingredient.count,
               name: ingredient.name,
               units: ingredient.units,
             };
@@ -40,6 +43,7 @@ export default function ListPage() {
           listReq.count += recipeReq.count;
         });
       });
+      shoppingList = shoppingList.filter((item) => item.count > 0);
       setListState(shoppingList);
       return shoppingList;
     },
@@ -54,28 +58,65 @@ export default function ListPage() {
 
   const [listState, setListState] = useState<ShoppingListReq[] | null>(null);
 
+  const addToStock = useMutation({
+    async mutationFn() {
+      if (!listState) return;
+      for (let i = 0; i < listState.length; i++) {
+        const listItem = listState[i];
+        await db.ingredients.update(
+          {
+            count: db.ingredients.util.increment(listItem.count),
+          },
+          listItem.key,
+        );
+      }
+    },
+    onSuccess() {
+      queryClient.invalidateQueries({ queryKey: ["stock"] });
+      navigate("/stock");
+    },
+  });
+
+  const [modal, setModal] = useState(false);
+
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
   return (
     <>
+      <Modal
+        action={addToStock}
+        prompt="Are you sure you want to add this shopping list to your stock?"
+        show={modal}
+        setShow={setModal}
+      />
       <Title>List</Title>
       <div className="grid h-full w-full grid-rows-[min-content,1fr] gap-5">
         <div>
-          <Button>
+          <Button
+            disabled={!(listState && listState.length > 0)}
+            onClick={() => setModal(true)}
+          >
             <FiShoppingCart />
             Add to stock
           </Button>
         </div>
         {isLoading && <Loader />}
-        {shoppingList.isSuccess && listState && (
-          <ul className="flex h-full w-full list-disc flex-col gap-2 overflow-y-scroll px-5 py-1">
-            {listState.map((requirement) => (
-              <ShoppingListItem
-                key={requirement.key}
-                item={requirement}
-                setListState={setListState}
-              />
-            ))}
-          </ul>
-        )}
+        {shoppingList.isSuccess &&
+          listState &&
+          (listState.length > 0 ? (
+            <ul className="flex h-full w-full list-disc flex-col gap-2 overflow-y-scroll px-5 py-1">
+              {listState.map((requirement) => (
+                <ShoppingListItem
+                  key={requirement.key}
+                  item={requirement}
+                  setListState={setListState}
+                />
+              ))}
+            </ul>
+          ) : (
+            <Alert icon={FiSmile} message="You don't need to buy anything" />
+          ))}
       </div>
     </>
   );
