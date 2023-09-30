d16ec3340769f50f48666fe236eb8573c0276347
Zuma <zuma@zuma.eu.org>
2023-09-30 14:13:23 +0100

    Added editing stock count

--- src/components/IngredientOpts.tsx
+++ src/components/IngredientOpts.tsx
@@ -5,6 +5,8 @@ import db, { Ingredient } from "@/lib";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import Modal from "./Modal";
 import { useState } from "react";
+import InputField from "./InputField";
+import { onNumberChange } from "@/lib/binding";
 
 type Props = {
   ingredient: OutputRecord<Ingredient>;
@@ -24,7 +26,27 @@ export default function IngredientOpts(props: Props) {
     },
   });
 
+  const editCount = useMutation({
+    async mutationFn() {
+      if (typeof count == "string") return;
+      await db.ingredients.update(
+        {
+          count: count,
+        },
+        props.ingredient.key,
+      );
+    },
+    onSuccess() {
+      queryClient.invalidateQueries({
+        predicate: ({ queryKey }) => queryKey.includes("stock"),
+      });
+    },
+  });
+
   const [deleteModal, setDeleteModal] = useState(false);
+
+  const [editModal, setEditModal] = useState(false);
+  const [count, setCount] = useState<string | number>(props.ingredient.count);
 
   return (
     <>
@@ -34,7 +56,20 @@ export default function IngredientOpts(props: Props) {
         show={deleteModal}
         setShow={setDeleteModal}
       />
-      <Button>
+      <Modal
+        action={editCount}
+        prompt={
+          <InputField
+            type="number"
+            step={0.1}
+            value={count}
+            onChange={onNumberChange(setCount)}
+          />
+        }
+        show={editModal}
+        setShow={setEditModal}
+      />
+      <Button onClick={() => setEditModal(true)}>
         <FiEdit />
       </Button>
       <Button onClick={() => setDeleteModal(true)}>
