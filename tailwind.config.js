1fdc5a675dc57fee9d96dfae6186a5367ccc3200
Zuma <zuma@zuma.eu.org>
2023-10-02 13:15:41 +0100

    animated modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadein: "fadein .2s linear forwards",
+        floatup: "floatup .2s linear forwards",
+      },
+      keyframes: {
+        fadein: {
+          "0%": {
+            opacity: "0%",
+          },
+          "100%": {
+            opacity: "100%",
+          },
+        },
+        floatup: {
+          "0%": {
+            transform: "translateY(2rem)",
+          },
+          "100%": {
+            transform: "translateY(0rem)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
